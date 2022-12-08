787f78227e756490c76677c4269bd8d669618520
USER <rajputshubham615@gmail.com>
2022-12-08 12:32:17 +0530

    added the properties and pom dependencies annotations

--- BEJ_C2_S3_REST_API_MONGODB_PC_1/src/main/java/com/example/BEJ_C2_S3_REST_API_MONGODB_PC_1/BejC2S3RestApiMongodbPc1Application.java
+++ BEJ_C2_S3_REST_API_MONGODB_PC_1/src/main/java/com/example/BEJ_C2_S3_REST_API_MONGODB_PC_1/BejC2S3RestApiMongodbPc1Application.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class BejC2S3RestApiMongodbPc1Application {
-
     public static void main(String[] args) {
         SpringApplication.run(BejC2S3RestApiMongodbPc1Application.class, args);
     }
